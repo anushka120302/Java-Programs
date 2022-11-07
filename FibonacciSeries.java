f1b5acb6d206cc8db4dbc9bef2210fcfe5e8cdcb
Anushka Agarwal <87578160+anushka120302@users.noreply.github.com>
2022-11-07 10:42:26 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,7 +3,7 @@ public class FibonacciSeries
 {
    public static void main(String args[])
    {
-	   Scanner sc=new Scanner(System.in);
+	  Scanner sc=new Scanner(System.in);
 	  int n1=0, n2=1, n3=0;
 	  int i=0;
 	  System.out.println("Enter the value of n");
@@ -16,5 +16,5 @@ public class FibonacciSeries
 		  n2=n3;
 		  System.out.print(" , "+n3);
 	  }
-   }
+     }
 }
