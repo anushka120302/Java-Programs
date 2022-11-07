3251895cf9d637706da12cb3ee4457a46d4b7bca
Anushka Agarwal <87578160+anushka120302@users.noreply.github.com>
2022-11-07 10:46:15 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -9,7 +9,7 @@ public class ReverseString
         String temp="";
         for(int i=s.length()-1;i>=0;i--)
         {
-        	temp=temp+(s.charAt(i));
+            temp=temp+(s.charAt(i));
         }
         System.out.println(temp);
 		
